fdbad29e2d154da45ec2079bf41aec6323c1dacc
sebi <sebi@hypertux>
2018-03-04 01:01:39 +0100

    Added batch example

---
+++ bootysoap/src/main/java/de/sebikopp/bootysoap/batch/control/BatchConf.java
@@ -0,0 +1,94 @@
+package de.sebikopp.bootysoap.batch.control;
+
+import java.nio.file.Paths;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Arrays;
+
+import javax.sql.DataSource;
+
+import org.apache.logging.log4j.Logger;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.ItemPreparedStatementSetter;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.PathResource;
+import de.sebikopp.bootysoap.batch.entity.Person;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConf {
+	@Autowired
+	JobBuilderFactory jobBuilderFactory;
+	
+	@Autowired
+	StepBuilderFactory stepBuilderFactory;
+	
+	@Autowired
+	DataSource dataSource;
+	
+	@Autowired
+	Logger logger;
+	
+	@Autowired
+	PersonProcessor processor;
+	
+	FlatFileItemReader<Person> reader() {
+		FlatFileItemReader<Person> reader = new FlatFileItemReader<>();
+		reader.setResource(new PathResource(Paths.get("testdata", "sample.csv")));
+//		reader.setLinesToSkip(linesToSkip);
+		reader.setLineMapper((lineContent, lineNumber) -> {
+			String[] split = lineContent.split("[;,]");
+			if (split.length == 2 && Arrays.stream(split).noneMatch(e -> e == null || e.isEmpty())) {
+				Person person = new Person(split[0], split[1]);
+				logger.debug("Person successfully read: {}", person);
+				return person;
+			} else {
+				logger.warn("Invalid line: {}. Result will be null.", lineContent);
+				return null;
+			}
+		});
+		return reader;
+	}
+	
+	JdbcBatchItemWriter<Person> writer() {
+		JdbcBatchItemWriter<Person> writer = new JdbcBatchItemWriter<>();
+		writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>());
+		writer.setSql("INSERT INTO people (first_name, last_name) VALUES (:firstName, :lastName);");
+		writer.setItemPreparedStatementSetter((pers, prepStat) -> {
+			prepStat.setString(1, pers.getFirstName());
+			prepStat.setString(2, pers.getLastName());
+		});
+		writer.setDataSource(dataSource);
+		return writer;
+	}
+	
+	@Bean
+	public Job importUserJob(ExampleCompletionListener completionListener) {
+		return jobBuilderFactory.get("importUserJob")
+				.incrementer(new RunIdIncrementer())
+				.listener(completionListener)
+				.flow(step1())
+				.end()
+				.build();
+	}
+	
+	@Bean
+	public Step step1() {
+		return stepBuilderFactory.get("step1")
+				.<Person,Person>chunk(10)
+				.reader(reader())
+				.processor(processor)
+				.writer(writer())
+				.build();
+	}
+}
---
+++ bootysoap/src/main/java/de/sebikopp/bootysoap/batch/entity/Person.java
@@ -0,0 +1,30 @@
+package de.sebikopp.bootysoap.batch.entity;
+
+import java.util.Objects;
+
+public class Person {
+	private String firstName;
+	private String lastName;
+	
+	protected Person() {
+	}
+
+	public Person(String firstName, String lastName) {
+		super();
+		this.firstName = Objects.requireNonNull(firstName);
+		this.lastName = Objects.requireNonNull(lastName);
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+}
