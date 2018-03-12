e402321c5b2705ff0c2399bb87b3b77fff3ffce0
sebi <sebi@hypertux>
2018-03-12 22:39:28 +0100

    Added injection of WsContext

--- bootysoap/src/main/java/de/sebikopp/bootysoap/soap/DigestWSImpl.java
+++ bootysoap/src/main/java/de/sebikopp/bootysoap/soap/DigestWSImpl.java
@@ -8,7 +8,9 @@ import java.security.NoSuchAlgorithmException;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
+import javax.annotation.Resource;
 import javax.jws.WebService;
+import javax.xml.ws.WebServiceContext;
 
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,12 +33,16 @@ public class DigestWSImpl implements DigestWebservice{
 	@Autowired
 	Logger logger;
 	
+	@Resource
+	WebServiceContext webServiceContext;
+	
 	static final String COMP_INJECT_NAME_DIGEST_WS = "DigestWebservice";
 	
 	@Override
 	public CreateDigestResponse getDigest(CallID header1, CreateDigestRequest body) throws CustomFault {
 		final String algorithm = body.getAlgorithm();
 		try {
+			logger.debug("Msg ctx: {}", webServiceContext.getMessageContext());
 			byte[] digest = MessageDigest.getInstance(algorithm).digest(body.getPayload());
 			CreateDigestResponse response = new CreateDigestResponse();
 			response.setPayloadDigest(digest);
@@ -53,6 +59,7 @@ public class DigestWSImpl implements DigestWebservice{
 	@Override
 	public void pushPayload(CallID hdx, PushPayloadRequest body) {
 		try {
+			logger.debug("Msg ctx: {}", webServiceContext.getMessageContext());
 			logger.debug("Starting to sleep ...");
 			TimeUnit.SECONDS.sleep(42);
 			logger.debug("Finished with sleeping ...");
